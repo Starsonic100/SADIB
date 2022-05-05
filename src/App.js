551b71258e4aad070d4a761ca393e5e4ad23cc91
Alexia <alexiamrp@hotmail.com>
2022-05-04 22:56:53 -0500

    App.js e index.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import MainP from './MainP';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/NavBar';
+import Principal from './components/Principal/PrincipalPage';
+import TAMAI from './components/TAMAI/TAMAIPage';
+import HTP from './components/HTP/HTPPage';
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
                       <Navbar/>
                       
             <Routes>
-              <Route exact path="/" element={<MainP/>} />
+              {/* <Route exact path="/" element={<MainP/>} />*/}
+              <Route path='/' element={<Principal/>}/>
+              <Route path='/TAMAI' element={<TAMAI/>}/>
+              <Route path='/HTP' element={<HTP/>}/>
            </Routes>
 
         </Router>
