ad7ad83cf0e42f356cc386f3381e479d6f7fca81
Alexia <alexiamrp@hotmail.com>
2022-05-10 15:40:25 -0500

    Se agregó navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import './App.css';
-import MainP from './MainP';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/NavBar';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Principal from './components/Principal/PrincipalPage';
 import TAMAI from './components/TAMAI/TAMAIPage';
 import HTP from './components/HTP/HTPPage';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    
-        <Router>
-                      <Navbar/>
-                      
-            <Routes>
-              {/* <Route exact path="/" element={<MainP/>} />*/}
-              <Route path='/' element={<Principal/>}/>
-              <Route path='/TAMAI' element={<TAMAI/>}/>
-              <Route path='/HTP' element={<HTP/>}/>
-           </Routes>
-
-        </Router>
-  ); 
+    <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Principal/>}/>
+          <Route path='/TAMAI' element={<TAMAI/>}/>
+          <Route path='/HTP' element={<HTP/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 
 export default App;
