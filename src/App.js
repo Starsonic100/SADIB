63e30edc6471d08d47ace199fb9f19e289f77898
Alexia <alexiamrp@hotmail.com>
2022-05-11 22:18:14 -0500

    Se agregó LogIn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import Principal from './components/Principal/PrincipalPage';
 import TAMAI from './components/TAMAI/TAMAIPage';
 import HTP from './components/HTP/HTPPage';
-import Navbar from './components/Navbar/Navbar';
+import SignLog from './components/SignUp-LogIn/cliente/SignUp-LogIn'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Principal/>}/>
+          <Route path='/SignUp-LogIn' element={<SignLog/>}/>
           <Route path='/TAMAI' element={<TAMAI/>}/>
           <Route path='/HTP' element={<HTP/>}/>
         </Routes>
