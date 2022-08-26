ad102752df49524f668ba9a36d0ce785505e3f97
Alexia <alexiamrp@hotmail.com>
2022-08-25 19:47:14 -0500

    API para actualización de datos

--- src/components/Pacientes/servidor.js
+++ src/components/Pacientes/servidor.js
@@ -13,10 +13,45 @@ const db = mysql.createConnection({
   database: "sadib",
 });
 
+function getId(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 app.post("/create", (req,res) =>{
 
   //Datos del tutor
-  const id_tutor = Math.floor(Math.random() * 9999);
+  const id_tutor = getId(100000,999999);
+  const tnombre = req.body.tnombre
+  const tpapellido = req.body.tpapellido
+  const tsapellido = req.body.tsapellido
+  const temail = req.body.temail
+  const ttelefono = req.body.ttelefono
+
+  //Datos del paciente
+  const id_paciente=getId(100000,999999);
+  const nombre = req.body.pnombre
+  const papellido = req.body.ppapellido
+  const sapellido = req.body.psapellido
+  const fecha_nac = req.body.pfnacimiento
+  const genero = req.body.pgenero
+  const email = req.body.pemail
+  const telefono = req.body.ptelefono
+
+  db.query(
+    "INSERT INTO tutor (id_tutor,nombre,apellidop,apellidom,correo,telefono) VALUES(?,?,?,?,?,?)",[id_tutor,tnombre,tpapellido,tsapellido,temail,ttelefono],(err,result) => { console.log(err); }
+  );
+
+  db.query(
+    "INSERT INTO paciente (id_paci,nombre,apellidop,apellidom,fecha_nac,genero,correo,telefono,id_tutor) VALUES(?,?,?,?,?,?,?,?,?)",[id_paciente,nombre,papellido,sapellido,fecha_nac,genero,email,telefono,id_tutor],(err,result) => { console.log(err); }
+  );
+  
+});
+
+app.post("/update", (req,res) =>{
+
+  const id_paciente=301909;
+
+  //Datos del tutor
   const tnombre = req.body.tnombre
   const tpapellido = req.body.tpapellido
   const tsapellido = req.body.tsapellido
@@ -33,11 +68,11 @@ app.post("/create", (req,res) =>{
   const telefono = req.body.ptelefono
 
   db.query(
-    "INSERT INTO tutor (id_tutor,nombre,apellidop,apellidom,correo,telefono) VALUES(?,?,?,?,?,?)",[id_tutor,tnombre,tpapellido,tsapellido,temail,ttelefono],(err,result) => { console.log(err); }
+    "UPDATE tutor SET nombre=?,apellidop=?,apellidom=?,correo=?,telefono=? WHERE id_tutor IN (SELECT id_tutor FROM paciente WHERE id_paci=?)",[tnombre,tpapellido,tsapellido,temail,ttelefono,id_paciente],(err,result) => { console.log(err); }
   );
 
   db.query(
-    "INSERT INTO paciente (nombre,apellidop,apellidom,fecha_nac,genero,correo,telefono,id_tutor) VALUES(?,?,?,?,?,?,?,?)",[nombre,papellido,sapellido,fecha_nac,genero,email,telefono,id_tutor],(err,result) => { console.log(err); }
+    "UPDATE paciente SET nombre=?,apellidop=?,apellidom=?,fecha_nac=?,genero=?,correo=?,telefono=? WHERE id_paci=?",[nombre,papellido,sapellido,fecha_nac,genero,email,telefono,id_paciente],(err,result) => { console.log(err); }
   );
   
 });
