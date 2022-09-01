7d8aaa297de6ad6c713aff9cf2d5f760987e48aa
Alexia <alexiamrp@hotmail.com>
2022-08-31 19:35:33 -0500

    Actualización de página principal

--- src/components/Principal/PrincipalPage.js
+++ src/components/Principal/PrincipalPage.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import '../css/style.css';
 import ninio from '../img/ninio.png';
 import escribe from '../img/escribe.jpg';
+import sesion from '../img/sesion.png';
+import agregar from '../img/agregar.png';
+import seleccionar from '../img/seleccionar.png';
+import paciente from '../img/paciente.png';
+import prediagnostico from '../img/prediagnostico.png';
+import expediente from '../img/expediente.png';
 import{ createTheme, MuiThemeProvider, responsiveFontSizes, Typography} from "@material-ui/core";
 
 let theme = createTheme();
@@ -47,7 +53,23 @@ class PrincipalPage extends Component {
 								<div className="main row">
 									<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
 									 	<div align="center">
-										 	descripción
+										 	<p>
+												<MuiThemeProvider theme={theme}>
+													<Typography variant="h6" gutterBottom class="desc">{"ACERCA DEL SITIO"}</Typography>
+												</MuiThemeProvider>
+											</p>
+										</div>
+										<div align="justify">
+											<p>
+												<MuiThemeProvider theme={theme}>
+													<Typography variant="h6" gutterBottom class="desc">{"Este sitio va dirigido a psicólogos. A través de él, podrán registrar a sus pacientes y aplicar en ellos pruebas como HTP (House-Tree-Person) y Test TAMAI."}</Typography>
+												</MuiThemeProvider>
+											</p>
+											<p>
+												<MuiThemeProvider theme={theme}>
+													<Typography variant="h6" gutterBottom class="desc">{"El prediagnóstico obtenido con la plataforma podrá ser revisado, editado y guardado por el especialista."}</Typography>
+												</MuiThemeProvider>
+											</p>
 										</div>
 									</div>
 									<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
@@ -57,7 +79,131 @@ class PrincipalPage extends Component {
 							</div>
 						</div>
 					</div>
+				</div>
+
+				<div className="main row">
+					<div className="col-12">
+						<div className="container">
+							<div className="funcion">
+								<div className="main row">
+									<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+									 	<div align="center">
+										 	<p>
+												<MuiThemeProvider theme={theme}>
+													<Typography variant="h1" gutterBottom class="func">{"¿Cómo funciona?"}</Typography>
+												</MuiThemeProvider>
+											</p>
+										</div>
+									</div>
+								</div>
+
+								<div className="main row">
+									<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+									 	<div className="cuadro">
+											<div align="center">
+												<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div align="center"><img src={sesion} width="60%"/></div>
+												</div>
+												<br></br>
+												<p>
+													<MuiThemeProvider theme={theme}>
+														<Typography variant="h3" gutterBottom class="cuad">{"El psicólogo inicia sesión"}</Typography>
+													</MuiThemeProvider>
+												</p>
+											</div>
+										</div>
+									</div>
+
+									<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+									 	<div className="cuadro">
+											<div align="center">
+												<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div align="center"><img src={agregar} width="60%"/></div>
+												</div>
+												<br></br>
+												<p>
+													<MuiThemeProvider theme={theme}>
+														<Typography variant="h3" gutterBottom class="cuad">{"Registra a sus pacientes"}</Typography>
+													</MuiThemeProvider>
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="main row">
+									<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+									 	<div className="cuadro">
+											<div align="center">
+												<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div align="center"><img src={seleccionar} width="60%"/></div>
+												</div>
+												<br></br>
+												<p>
+													<MuiThemeProvider theme={theme}>
+														<Typography variant="h3" gutterBottom class="cuad">{"Elige las pruebas a aplicar y genera código"}</Typography>
+													</MuiThemeProvider>
+												</p>
+											</div>
+										</div>
+									</div>
+
+									<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+									 	<div className="cuadro">
+											<div align="center">
+												<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div align="center"><img src={paciente} width="60%"/></div>
+												</div>
+												<br></br>
+												<p>
+													<MuiThemeProvider theme={theme}>
+														<Typography variant="h3" gutterBottom class="cuad">{"El paciente ingresa el código y realiza las pruebas"}</Typography>
+													</MuiThemeProvider>
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="main row">
+									<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+									 	<div className="cuadro">
+											<div align="center">
+												<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div align="center"><img src={prediagnostico} width="60%"/></div>
+												</div>
+												<br></br>
+												<p>
+													<MuiThemeProvider theme={theme}>
+														<Typography variant="h3" gutterBottom class="cuad">{"Se obtiene el prediagnóstico"}</Typography>
+													</MuiThemeProvider>
+												</p>
+											</div>
+										</div>
+									</div>
+
+									<div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+									 	<div className="cuadro">
+											<div align="center">
+												<div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div align="center"><img src={expediente} width="60%"/></div>
+												</div>
+												<br></br>
+												<p>
+													<MuiThemeProvider theme={theme}>
+														<Typography variant="h3" gutterBottom class="cuad">{"El psicólogo puede ver, editar y guardar los resultados"}</Typography>
+													</MuiThemeProvider>
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
 				</div>	
+
             </div>
         );
     }
