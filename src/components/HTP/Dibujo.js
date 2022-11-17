09a0e173cd869da2f1955c3077ffd960c89b6418
Starsonic100 <sonic.spider98@hotmail.com>
2022-11-17 14:44:23 -0600

    Fondo

--- src/components/HTP/Dibujo.js
+++ src/components/HTP/Dibujo.js
@@ -124,6 +124,7 @@ export class Dibujo extends Component {
                 let canvas=document.getElementById("canvas");
                 let context=canvas.getContext("2d");
                 context.clearRect(0,0,canvas.width,canvas.height);
+                context.fillStyle = "white";
             }
 
             const setToDownload = () =>{
