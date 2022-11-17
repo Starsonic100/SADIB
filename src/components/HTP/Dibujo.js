8e39f881776ce0960d1953e95fd4347ae8a00ac1
Starsonic100 <sonic.spider98@hotmail.com>
2022-11-17 11:39:18 -0600

    Escala

--- src/components/HTP/Dibujo.js
+++ src/components/HTP/Dibujo.js
@@ -135,7 +135,7 @@ export class Dibujo extends Component {
 
                 let canvas=document.getElementById("canvas");
                 
-                resizedContext.drawImage(canvas, 0, 0, 500, 500);
+                resizedContext.drawImage(canvas, 0, 0, 100, 100);
                 let image = resizedCanvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
                 let link = document.createElement('a');
                 link.download = "my-image.png";
