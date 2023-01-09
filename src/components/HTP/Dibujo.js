1656c3cc604053b6f7eb024cf3ffd17f1d89d053
Starsonic100 <sonic.spider98@hotmail.com>
2023-01-09 12:12:07 -0600

    Spinner load

--- src/components/HTP/Dibujo.js
+++ src/components/HTP/Dibujo.js
@@ -164,7 +164,8 @@ export class Dibujo extends Component {
             const uploadFile = () => {
                 let botonFinal=document.getElementById("finalizar");
                 let carga=document.getElementById("cargaDibujo");
-                botonFinal.setAttribute("hidden","hidden");
+                botonFinal.hidden="true";
+                botonFinal.disabled="true";
                 carga.removeAttribute("hidden");
                 let resizedCanvas = document.createElement("canvas");
                 let resizedContext = resizedCanvas.getContext("2d");
@@ -193,6 +194,7 @@ export class Dibujo extends Component {
             function predecir() {
                 let botonFinal=document.getElementById("finalizar");
                 let carga=document.getElementById("cargaDibujo");
+                let continua=document.getElementById("continuar");
                 let resizedCanvas = document.createElement("canvas");
                 let resizedContext = resizedCanvas.getContext("2d");
                 resizedCanvas.height = "250";
@@ -224,12 +226,16 @@ export class Dibujo extends Component {
                 // console.log(response);
                     console.log(response.data);
                     dibujos('rDc',response.data); 
-                    alert("Se ha cargado su dibujo");
+                    
                    
                 })
                 .catch((error)=> {
                     console.log(error)
                 });
+                carga.hidden="true";
+                botonFinal.removeAttribute("hidden");
+                alert("Se ha cargado su dibujo");
+                continua.click();
             }
 
             function resample_single(canvas, width, height, resize_canvas) {
